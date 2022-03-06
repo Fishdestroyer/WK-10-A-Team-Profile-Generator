db56301ac3fddb8153e653a42ee869d5af69f1e6
Fishdestroyer <jeffwhitner@gmail.com>
2022-03-06 00:28:45 -0600

    update html and css, updated employee test and constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,9 +3,15 @@ const Employee = require('../lib/Employee');
 test('creates an employee object', () => {
     const employee = new Employee('Employee', 'ID####', 'email@email.com');
 
-    expect(employee.name).toBe('Employee',);
+    expect(employee.name).toBe('name',);
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
+});  
+
+test('gets user name', () => {
+    const employee = new Employee('Employee', 'ID####', 'email@email.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
 });
 
-module.exports = Employee;
+
